d4dc1e332819326e9f42bada2ef35f195e470847
Ayo-Olonilua Ayomipe <139784960+AyoOlonilua@users.noreply.github.com>
2023-09-22 12:39:24 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business logic
+
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -12,16 +14,45 @@ function wordCounter(text) {
   return wordCount;
 }
 
-function numberOfOccurrencesInText(word, text) {
+function numberOfOccurrencesInText(text) {
   if (text.trim().length === 0) {
-    return 0;
+    return [];
   }
+
+  const wordOrder = [];
   const wordArray = text.split(" ");
-  let wordCount = 0;
+
   wordArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
+      const word = element.toLowerCase();
+
+    if (word.trim() !== "") {
+      if (wordOrder[word]) {
+          wordOrder[word]++;
+      } else {
+          wordOrder[word] = 1;
+      }
     }
   });
-  return wordCount;
+  return wordOrder;
 }
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    
+    const passage = $("#text-passage").val();
+    const wordCount = wordCounter(passage);
+    const wordFrequency = numberOfOccurrencesInText(passage);
+
+    for (const word in wordFrequency) {
+       wordFrequency.hasOwnProperty(word) 
+       const count = wordFrequency[word];
+        $("#word-list").append("<li>" + word + ": " + count + "</li>");
+      
+    }
+  
+  $("#total-count").html(wordCount);
+  });
+});
